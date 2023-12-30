382a05cb36e088637eac616ac9818af43cdbbed5
Usa107ma <you@example.com>
2023-12-30 17:41:57 +0500

    Add AnotherComponent file

---
+++ AnotherComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+ AnotherComponent = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>This is the text from AnotherComponent!</Text>
+    </View>
+  );
+};
+
+export default AnotherComponent;
+
